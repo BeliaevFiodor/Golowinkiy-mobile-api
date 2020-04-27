9ac5436da184d59db162422cc03857e6eec996cb
Fiodor <fedorbel79@gmail.com>
2020-04-27 10:40:35 +0300

    Routes update

--- golowinsky-mobile/Controllers/MainController.cs
+++ golowinsky-mobile/Controllers/MainController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace golowinsky_mobile.Controllers
 {
     [Produces("application/json")]
-    [Route("api/")]
+    [Route("ws/carprc.svc/")]
     [ApiController]
     [AllowAnonymous]
     public class MainController : ControllerBase
